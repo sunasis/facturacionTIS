05ec5e6191dad7ec37dcfad8fab84b07b53632ae
Giancarlos Salas <giansalex@gmail.com>
2017-06-03 17:39:06 -0500

    Add verify xml without certificate File

--- FacturacionElectronica.GeneradorXml/XmlSignatureProvider.cs
+++ FacturacionElectronica.GeneradorXml/XmlSignatureProvider.cs
@@ -10,11 +10,9 @@
 // and place it in the store.
 // makecert -r -pe -n "CN=XMLDSIG_Test" -b 01/01/2005 -e 01/01/2010 -sky signing -ss my
 
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+using System;
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Cryptography.Xml;
-using System.Text;
 using System.Xml;
 
 namespace FacturacionElectronica.GeneradorXml
@@ -59,22 +57,27 @@ namespace FacturacionElectronica.GeneradorXml
             doc.ReplaceChild(xmlDeclaration, doc.FirstChild);
         }
 
-#if DEBUG
-        /// <summary>
-        /// Verifica que el archivo ZML este correctamente firmado
-        /// </summary>
-        /// <param name="fileName">Path of XML File</param>
-        /// <param name="certificado">Certificado Digital</param>
-        /// <returns></returns>
-        [SuppressMessage("ReSharper", "UnusedMember.Local")]
-        private static bool VerifyXmlFile(string fileName, X509Certificate2 certificado)
+        public static bool VerifyXmlFile(string fileName)
         {
             var xmlDocument = new XmlDocument();
             xmlDocument.Load(fileName);
             var signedXml = new SignedXml(xmlDocument);
             var nodeList = xmlDocument.GetElementsByTagName("ds:Signature");
-            signedXml.LoadXml((XmlElement)nodeList[0]);
-            return signedXml.CheckSignature(certificado, true);
+            var certNode = xmlDocument.GetElementsByTagName("ds:X509Certificate");
+            if (certNode.Count == 0)
+                certNode = xmlDocument.GetElementsByTagName("X509Certificate");
+
+            try
+            {
+                var cert = new X509Certificate2(Convert.FromBase64String(certNode[0].InnerText));
+                signedXml.LoadXml((XmlElement)nodeList[0]);
+
+                return signedXml.CheckSignature(cert, true);
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         /*
@@ -128,43 +131,5 @@ namespace FacturacionElectronica.GeneradorXml
         }
          */
 
-        public static KeyValuePair<bool, string> SignXmlFileTest(string signedFileName, X509Certificate2 cert)
-        {
-            var doc = new XmlDocument();
-            doc.Load(signedFileName);
-            doc.DocumentElement.SetAttribute("xmlns:ds", SignedXml.XmlDsigNamespaceUrl);
-            var signedXml = new SignedXml(doc) { SigningKey = cert.PrivateKey };
-            var reference = new Reference { Uri = "" };
-
-            var env = new XmlDsigEnvelopedSignatureTransform();
-            reference.AddTransform(env);
-            signedXml.AddReference(reference);
-            var keyInfo = new KeyInfo();
-            var x509KeyInfo = new KeyInfoX509Data(cert, X509IncludeOption.WholeChain);
-
-            keyInfo.AddClause(x509KeyInfo);
-            signedXml.KeyInfo = keyInfo;
-            signedXml.Signature.Id = "SignatureSP";
-            signedXml.ComputeSignature();
-            var xmlDigitalSignature = signedXml.GetXml();
-            xmlDigitalSignature.Prefix = "ds";
-            var nameSpace = new XmlNamespaceManager(doc.NameTable);
-            nameSpace.AddNamespace("tns", "urn:sunat:names:specification:ubl:peru:schema:xsd:SummaryDocuments-1"); // Namespace del Xml (Cambiar)
-            nameSpace.AddNamespace("ext", "urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2");
-            var signNodes = doc.SelectNodes("/tns:SummaryDocuments/ext:UBLExtensions/ext:UBLExtension/ext:ExtensionContent", nameSpace); // PathExtions del Xml (Cambiar)
-
-            if (signNodes != null && signNodes.Count > 0)
-                signNodes[signNodes.Count - 1].AppendChild(doc.ImportNode(xmlDigitalSignature, true));
-
-            doc.ReplaceChild(doc.CreateXmlDeclaration("1.0", "ISO-8859-1", "no"), doc.FirstChild);
-            var pathResult = System.IO.Path.Combine(System.IO.Path.GetTempPath(), System.IO.Path.GetFileName(signedFileName));
-            using (var xmltw = new XmlTextWriter(pathResult, Encoding.GetEncoding("iso-8859-1")))
-            {
-                doc.WriteTo(xmltw);
-                xmltw.Close();
-            }
-            return new KeyValuePair<bool, string>(VerifyXmlFile(pathResult, cert), pathResult);
-        }
-#endif
     }
 }
