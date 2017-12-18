1ec460c3fdaef15c70ed1292ecc1bbd59707bab6
Giancarlos Salas <giansalex@gmail.com>
2017-12-18 11:12:14 -0500

    Add validator (#5)
    * Add async methods to Other Docs + DespatchAdvice Tests
    
    * Add UBL v2 Project
    
    * Add driver Person to Guia Remision + Complete Test (guia  remision)
    
    * Skip unnecessary nodes
    
    * Add custom error soap exceptio in Other Ce
    
    * Fix errors code xml
    
    * Remove invalid code errors
    
    * Add ext xml Namespace
    
    * Add support anticipo, guia embed + Add tipo operacion SUNAT + deploy as nuget package
    
    * ubl nuget package + update dependency in xml generator
    
    * Add Fields in SunatEmbed
    
    * Update packages XML
    
    * update from v1, add summary v1, v2
    
    * update nuget packages
    
    * add validator, remove retencion project
    
    * add nuspec validador
    
    * Add validator Test
    
    * add unitests validators

---
+++ FacturacionElectronica.ValidadorTests/Sale/InvoiceValidatorTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using FacturacionElectronica.GeneradorXml.Entity;
+using FacturacionElectronica.GeneradorXml.Entity.Details;
+using FacturacionElectronica.GeneradorXml.Entity.Misc;
+using FacturacionElectronica.GeneradorXml.Enums;
+using FacturacionElectronica.Validador.Sale;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FacturacionElectronica.ValidadorTests.Sale
+{
+    [TestClass]
+    public class InvoiceValidatorTests
+    {
+        [TestMethod]
+        public void InvoiceValidatorTest()
+        {
+            var invoice = CreateInvoice();
+
+            IValidator validator = new InvoiceValidator();
+            var result = validator.Validate(invoice);
+
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [TestMethod]
+        public void InvoicenValidatorNoValidTest()
+        {
+            var invoice = CreateInvoice();
+
+            invoice.SerieDocumento = "G001";
+
+            IValidator validator = new InvoiceValidator();
+            var result = validator.Validate(invoice);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+        }
+
+        private InvoiceHeader CreateInvoice()
+        {
+            var invoice = new InvoiceHeader
+            {
+                TipoDocumento = TipoDocumentoElectronico.Factura,
+                SerieDocumento = "F001",
+                CorrelativoDocumento = "00005214",
+                FechaEmision = DateTime.Now.Subtract(TimeSpan.FromDays(2)),
+                NombreRazonSocialCliente = "SUPERMERCADOS Ñro <xml> íó PERUANOS SOCIEDAD ANóNIMA 'O ' S.P.S.A.",
+                NroDocCliente = "20100070970",
+                RucEmisor = "20600995805",
+                NombreRazonSocialEmisor = "ABLIMATEX EXPORT SAC",
+                CodigoMoneda = "PEN",
+                NombreComercialEmisor = "C-ABLIMATEX EXPORT SAC",
+                DocumentoReferenciaNumero = "F001-2233",
+                DocumentoReferenciaTipoDocumento = TipoDocumentoElectronico.Factura,
+                TipoDocumentoIdentidadCliente = TipoDocumentoIdentidad.RegistroUnicoContribuyentes,
+                TipoDocumentoIdentidadEmisor = TipoDocumentoIdentidad.RegistroUnicoContribuyentes,
+                GuiaRemisionReferencia = new List<GuiaRemisionType>
+                {
+                    new GuiaRemisionType {IdTipoGuiaRemision = "09", NumeroGuiaRemision = "0001-111111"}
+                },
+                DetallesDocumento = new List<InvoiceDetail>
+                {
+                    new InvoiceDetail
+                    {
+                        CodigoProducto = "PROD001",
+                        Cantidad = 120,
+                        DescripcionProducto = "PRODUCTO PRUEBA 001",
+                        PrecioUnitario = 1,
+                        PrecioAlternativos = new List<PrecioItemType>
+                        {
+                            new PrecioItemType
+                            {
+                                TipoDePrecio = TipoPrecioVenta.PrecioUnitario,
+                                Monto = 1.18M
+                            }
+                        },
+                        UnidadMedida = "NIU",
+                        ValorVenta = 120,
+                        Impuesto =
+                            new List<TotalImpuestosType>
+                            {
+                                new TotalImpuestosType {TipoTributo = TipoTributo.IGV_VAT, Monto = 21.60M, TipoAfectacion=TipoAfectacionIgv.GravadoOperacionOnerosa},
+                            },
+                    },
+                    new InvoiceDetail
+                    {
+                        CodigoProducto = "PROD002",
+                        Cantidad = 100,
+                        DescripcionProducto = "PRODUCTO PRUEBA 002",
+                        PrecioUnitario = 2,
+                        PrecioAlternativos = new List<PrecioItemType>
+                        {
+                            new PrecioItemType
+                            {
+                                TipoDePrecio = TipoPrecioVenta.PrecioUnitario,
+                                Monto = 2.36M
+                            }
+                        },
+                        UnidadMedida = "NIU",
+                        ValorVenta = 200,
+                        Impuesto = new List<TotalImpuestosType>
+                        {
+                            new TotalImpuestosType {TipoTributo = TipoTributo.IGV_VAT, Monto = 36M, TipoAfectacion = TipoAfectacionIgv.GravadoOperacionOnerosa},
+                        },
+                    },
+                },
+                DescuentoGlobal = 20.0m,
+                TotalVenta = 377.60M,
+                TotalTributosAdicionales = new List<TotalTributosType>
+                {
+                    new TotalTributosType
+                    {
+                        Id = OtrosConceptosTributarios.TotalVentaOperacionesGravadas,
+                        MontoPagable = 320M
+                    },
+                    new TotalTributosType
+                    {
+                        Id = OtrosConceptosTributarios.Detracciones,
+                        MontoPagable = 2208.962M,
+                        Porcentaje = 9.000M
+                    }
+                },
+                Impuesto = new List<TotalImpuestosType>
+                {
+                    new TotalImpuestosType {TipoTributo = TipoTributo.IGV_VAT, Monto = 57.6M},
+                    new TotalImpuestosType {TipoTributo = TipoTributo.ISC_EXC, Monto = 0M}
+                },
+                DireccionEmisor = new DireccionType
+                {
+                    CodigoUbigueo = "150111",
+                    Direccion = "AV. LOS PRECURSORES #1245",
+                    Zona = "URB. MIGUEL GRAU",
+                    Departamento = "LIMA",
+                    Provincia = "LIMA",
+                    Distrito = "EL AGUSTINO",
+                    CodigoPais = "PE"
+                }
+            };
+
+            return invoice;
+        }
+    }
+}
---
+++ FacturacionElectronica.ValidadorTests/Voided/VoidedValidatorTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using FacturacionElectronica.GeneradorXml.Entity;
+using FacturacionElectronica.GeneradorXml.Entity.Details;
+using FacturacionElectronica.GeneradorXml.Enums;
+using FacturacionElectronica.Validador.Voided;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FacturacionElectronica.ValidadorTests.Voided
+{
+    [TestClass]
+    public class VoidedValidatorTests
+    {
+        [TestMethod]
+        public void VoidedValidatorTest()
+        {
+            var voided = new VoidedHeader
+            {
+                TipoDocumentoIdentidadEmisor = TipoDocumentoIdentidad.RegistroUnicoContribuyentes,
+                RucEmisor = "20600995805",
+                FechaEmision = DateTime.Now.Subtract(TimeSpan.FromDays(2)),
+                NombreRazonSocialEmisor = "ABLIMATEX EXPORT SAC",
+                NombreComercialEmisor = "C-ABLIMATEX EXPORT SAC",
+                CorrelativoArchivo = "01",
+                DetallesDocumento = new List<VoidedDetail>
+                {
+                    new VoidedDetail{
+                        TipoDocumento = TipoDocumentoElectronico.Factura,
+                        SerieDocumento = "F001",
+                        CorrelativoDocumento = "1",
+                        Motivo = "ERROR EN SISTEMA",
+                    },
+                    new VoidedDetail{
+                        TipoDocumento = TipoDocumentoElectronico.Factura,
+                        SerieDocumento = "F001",
+                        CorrelativoDocumento = "15",
+                        Motivo = "CANCELACION"
+                    }
+                }
+            };
+            var validator = new VoidedValidator();
+            var result = validator.Validate(voided);
+            
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [TestMethod]
+        public void VoidedFailedValidatorTest()
+        {
+            var voided = new VoidedHeader
+            {
+                TipoDocumentoIdentidadEmisor = TipoDocumentoIdentidad.RegistroUnicoContribuyentes,
+                RucEmisor = "20600995805",
+                FechaEmision = DateTime.Now.Subtract(TimeSpan.FromDays(2)),
+                NombreRazonSocialEmisor = "ABLIMATEX EXPORT SAC",
+                NombreComercialEmisor = "C-ABLIMATEX EXPORT SAC",
+                CorrelativoArchivo = "01",
+                DetallesDocumento = new List<VoidedDetail>
+                {
+                    new VoidedDetail{
+                        TipoDocumento = TipoDocumentoElectronico.Factura,
+                        SerieDocumento = "G001",
+                        CorrelativoDocumento = "14567889",
+                        Motivo = "ERROR EN SISTEMA",
+                    },
+                    new VoidedDetail{
+                        TipoDocumento = TipoDocumentoElectronico.Factura,
+                        SerieDocumento = "F001",
+                        CorrelativoDocumento = "15",
+                        Motivo = "CANCELACION"
+                    }
+                }
+            };
+            IValidator validator = new VoidedValidator();
+            var result = validator.Validate(voided);
+
+            Assert.IsFalse(result.IsValid);
+
+            Trace.WriteLine(string.Join(Environment.NewLine, result.Errors.Select(r => r.ErrorMessage)));
+        }
+    }
+}
